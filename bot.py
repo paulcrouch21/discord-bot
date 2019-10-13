7d5c2a42f0e4aa1f331725b11d0d4484899dc18e
paulcrouch99 <paul.crouch99@yahoo.com>
2019-10-13 06:20:03 -0500

    Updated code for boy.py

--- bot.py
+++ bot.py
@@ -1,17 +1,37 @@
 #bot.py
 import os
-
-import discord
-import dotenv
+import random
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-token = os.getenv('discordToken')
+token = os.getenv("discordToken")
+guild = os.getenv("discordGuild")
 
-client = discord.Client()
+bot = commands.Bot(command_prefix = "!")
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
 
-client.run(token)
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f"Hi {member.name}, welcome to my Discord server! Please check the info channel for the rules")
+
+@bot.command(name = "99", help = "Responds with a random quote from Brooklyn 99")
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        "I\'m the human form of the 💯 emoji.",
+        "Cool, cool cool cool cool. No doubt no doubt no doubt.",
+        "A place where everybody knows your name is hell. You're describing hell.",
+        "The English language can not fully capture the depth and complexity of my thoughts, so I’m incorporating emojis into my speech to better express myself. Winky face.",
+        "If I die, turn my tweets into a book.",
+        "I asked them if they wanted to embarrass you, and they instantly said yes.",
+        "Good to see you. But if you’re here, who’s guarding Hades?"
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(token)
